e22a033b34220a085b5d4758cbc0cb655be49193
Elton Fagner <eltonfms@gmail.com>
2020-07-07 16:17:30 -0300

    grid container

--- src/pages/Styleguide/index.jsx
+++ src/pages/Styleguide/index.jsx
@@ -2,93 +2,98 @@ import React from 'react';
 
 const Styleguide = () => {
   return (
-    <>
-      <h1>Empresas Styleguide</h1> <br />
+    <div class="grid -container">
+      <div className="grid -wrap">
+        <div className="grid -col">
+          <h1>Empresas Styleguide</h1> 
+        </div>
+      </div>
 
-      <div>
-
-        <div className="logo">
+      <div className="grid -wrap">
+        <div className="grid -col">
           <div className="logo -home">
             <img src={require('../../assets/images/logo-home@2x.png')} alt="Ioasys" />
-          </div> <br />
+          </div>
+        </div>
+        <div className="grid -col">
           <div className="icons" style={{backgroundColor: '#000000', padding: '20px', }}>
             <div className="logo -nav">
               <img src={require('../../assets/images/logo-nav@2x.png')} alt="" />
             </div>
           </div>
         </div>
-
-        <div className="title">
-          <h1 className="title -larger">
-            Título Nível 1
-          </h1>
-          <h2 className="title -median">
-            Título Nível 2
-          </h2>
-          <h3 className="title -smaller">
-            Titúlo Nível 3
-          </h3>
-        </div>
-
-        <div className="icons" style={{backgroundColor: '#000000', padding: '50px', }}>
-          <i className="icon -back"></i> <br />
-          <i className="icon -close"></i> <br />
-          <i className="icon -eye"></i> <br />
-          <i className="icon -letter"></i> <br />
-          <i className="icon -padlock"></i> <br />
-          <i className="icon -search"></i>
-          
-        </div>
-
-        <div className="picture">
-          <div className="picture -placeholder">
-            <div className="picture -aspect d-388-147">
-              <div className="picture -figure">
-                <span>E1</span>
-              </div>
-            </div>
-          </div>
-          <br />
-          <div className="picture -placeholder">
-            <div className="picture -aspect d-293-160">
-              <div className="picture -figure">
-                <span>E1</span>
-              </div>
-            </div>
-          </div>
-          <br />
-          <div className="picture -placeholder">
-            <div className="picture -aspect d-293-160">
-              <div className="picture -figure">
-                <img src={require('../../assets/images/temp/temp-company.jpg')} alt="Empresa" />
-              </div>
-            </div>
-          </div>
-        </div>
-        
-        <div className="text">
-          <p className="text -larger">
-            A antiga Teoria dos Atos de Comércio decorrente da chamada codificação napoleônica nunca definiu muito bem o que eram as atividades mercantis, os chamados atos de comércio. A definição do que eram os atos de comércio não convenceu a doutrina, pois muitas atividades não eram consideradas comerciais por razões históricas, como era o caso da negociação de bens imobiliários. Além disso, com a constante inovação tecnológica do mercado, diversas novas atividades foram surgindo, mas não eram enumeradas como atos de comércio pela lentidão do processo legislativo.
-          </p>
-          <p className="text -median">
-            Entre para se manter melhor informado sobre o sistema Empresas.
-          </p>
-          <p className="text -smaller">
-            Credenciais informadas são inválidas, tente novamente.
-          </p>
-          <p className="text -info">
-            Clique na busca para iniciar.
-          </p>
-        </div>
-
-        <div className="button">
-          <button type="button" className="button -link -primary">
-            <span>Entrar</span>
-          </button>
-        </div>
-
       </div>
-    </>
+
+      <div className="title">
+        <h1 className="title -larger">
+          Título Nível 1
+        </h1>
+        <h2 className="title -median">
+          Título Nível 2
+        </h2>
+        <h3 className="title -smaller">
+          Titúlo Nível 3
+        </h3>
+      </div>
+
+      <div className="icons" style={{backgroundColor: '#000000', padding: '50px', }}>
+        <i className="icon -back"></i> <br />
+        <i className="icon -close"></i> <br />
+        <i className="icon -eye"></i> <br />
+        <i className="icon -letter"></i> <br />
+        <i className="icon -padlock"></i> <br />
+        <i className="icon -search"></i>
+        
+      </div>
+
+      <div className="picture">
+        <div className="picture -placeholder">
+          <div className="picture -aspect d-388-147">
+            <div className="picture -figure">
+              <span>E1</span>
+            </div>
+          </div>
+        </div>
+        <br />
+        <div className="picture -placeholder">
+          <div className="picture -aspect d-293-160">
+            <div className="picture -figure">
+              <span>E1</span>
+            </div>
+          </div>
+        </div>
+        <br />
+        <div className="picture -placeholder">
+          <div className="picture -aspect d-293-160">
+            <div className="picture -figure">
+              <img src={require('../../assets/images/temp/temp-company.jpg')} alt="Empresa" />
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      <div className="text">
+        <p className="text -larger">
+          A antiga Teoria dos Atos de Comércio decorrente da chamada codificação napoleônica nunca definiu muito bem o que eram as atividades mercantis, os chamados atos de comércio. A definição do que eram os atos de comércio não convenceu a doutrina, pois muitas atividades não eram consideradas comerciais por razões históricas, como era o caso da negociação de bens imobiliários. Além disso, com a constante inovação tecnológica do mercado, diversas novas atividades foram surgindo, mas não eram enumeradas como atos de comércio pela lentidão do processo legislativo.
+        </p>
+        <p className="text -median">
+          Entre para se manter melhor informado sobre o sistema Empresas.
+        </p>
+        <p className="text -smaller">
+          Credenciais informadas são inválidas, tente novamente.
+        </p>
+        <p className="text -info">
+          Clique na busca para iniciar.
+        </p>
+      </div>
+
+      <div className="button">
+        <button type="button" className="button -link -primary">
+          <span>Entrar</span>
+        </button>
+      </div>
+
+    </div>
   );
 };
 
