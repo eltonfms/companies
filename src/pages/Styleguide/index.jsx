abb2bcfa4435258b449f235857c33e99ecf686d0
Elton Fagner <eltonfms@gmail.com>
2020-07-07 11:11:26 -0300

    titles and paragraphs

--- src/pages/Styleguide/index.jsx
+++ src/pages/Styleguide/index.jsx
@@ -7,6 +7,7 @@ const Styleguide = () => {
 
       <div>
 
+        {/*
         <div className="logo">
           <div className="logo -main">
             <img src={require('../../assets/images/logo-home@2x.png')} alt="Ioasys" />
@@ -15,15 +16,16 @@ const Styleguide = () => {
             <img src={require('../../assets/images/logo-nav@2x.png')} alt="" />
           </div>
         </div>
+        */}
 
         <div className="title">
-          <h1 className="title -large">
+          <h1 className="title -larger">
             Título Nível 1
           </h1>
           <h2 className="title -median">
             Título Nível 2
           </h2>
-          <h3 className="title -small">
+          <h3 className="title -smaller">
             Titúlo Nível 3
           </h3>
         </div>
@@ -59,13 +61,13 @@ const Styleguide = () => {
         </div>
 
         <div className="text">
-          <p className="text -large">
+          <p className="text -larger">
             A antiga Teoria dos Atos de Comércio decorrente da chamada codificação napoleônica nunca definiu muito bem o que eram as atividades mercantis, os chamados atos de comércio. A definição do que eram os atos de comércio não convenceu a doutrina, pois muitas atividades não eram consideradas comerciais por razões históricas, como era o caso da negociação de bens imobiliários. Além disso, com a constante inovação tecnológica do mercado, diversas novas atividades foram surgindo, mas não eram enumeradas como atos de comércio pela lentidão do processo legislativo.
           </p>
           <p className="text -median">
             Entre para se manter melhor informado sobre o sistema Empresas.
           </p>
-          <p className="text -small">
+          <p className="text -smaller">
             Credenciais informadas são inválidas, tente novamente.
           </p>
           <p className="text -info">
