283a147dbc15a14e1997b7a1313c5e5a318a836a
Elton Fagner <eltonfms@gmail.com>
2020-07-07 10:24:52 -0300

    apply reset css

--- src/pages/Styleguide/index.jsx
+++ src/pages/Styleguide/index.jsx
@@ -4,6 +4,14 @@ const Styleguide = () => {
   return (
     <>
       <h1>Empresas Styleguide</h1>
+
+      <div>
+
+        <h1 className="title -large">Título Nível 1</h1>
+        <h2 className="title -median">Título Nível 2</h2>
+        <h3 className="title -small">Titúlo Nível 3</h3>
+
+      </div>
     </>
   );
 };
