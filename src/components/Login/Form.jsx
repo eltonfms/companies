21453d6189b85ef1984945d53b937c8949f5a751
Elton Fagner <eltonfms@gmail.com>
2020-07-07 20:30:28 -0300

    loading fixed layer

--- src/components/Login/Form.jsx
+++ src/components/Login/Form.jsx
@@ -15,61 +15,70 @@ const LoginForm = () => {
   };
 
   return (
-    <div className="login -form">
-      <form onSubmit={handleSubmit(onSubmit)} noValidate>
-        <div className="form -group w-350 m-auto">
-          <div className="form -item -email">
-            <i className="icon -letter"></i>
-            <input 
-              type="email" 
-              name="email" 
-              id="email"
-              placeholder="E-mail"
-              autocomplete="off"
-              ref={register({
-                required: 'Por favor preencha o e-mail.',
-                pattern: {
-                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                  message: "* E-mail inválido. Por favor preencha novamente."
-                }
-              })}
-            />
-            {errors.email &&
+    <>
+      <div className="login -form">
+        <form onSubmit={handleSubmit(onSubmit)} noValidate>
+          <div className="form -group w-350 m-auto">
+            <div className="form -item -email">
+              <i className="icon -letter"></i>
+              <input 
+                type="email" 
+                name="email" 
+                id="email"
+                placeholder="E-mail"
+                autoComplete="off"
+                ref={register({
+                  required: 'Por favor preencha o e-mail.',
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                    message: "* E-mail inválido. Por favor preencha novamente."
+                  }
+                })}
+              />
+              { errors.email &&
+                <div className="form -message -error">
+                  <p className="text -smaller c-alert">
+                    {errors.email.message}
+                  </p>
+                </div>
+              }
+            </div>
+            <div className="form -item -password">
+              <i className="icon -padlock"></i>
+              <input 
+                type="password" 
+                name="password" 
+                id="password"
+                placeholder="Senha"
+              />
+            </div>
+            { fail &&
               <div className="form -message -error">
                 <p className="text -smaller c-alert">
-                  {errors.email.message}
+                  Credenciais informadas são inválidas, tente novamente.
                 </p>
               </div>
             }
-          </div>
-          <div className="form -item -password">
-            <i className="icon -padlock"></i>
-            <input 
-              type="password" 
-              name="password" 
-              id="password"
-              placeholder="Senha"
-            />
-          </div>
-          { fail &&
-            <div className="form -message -error">
-              <p className="text -smaller c-alert">
-                Credenciais informadas são inválidas, tente novamente.
-              </p>
+            <div className="form -button m-auto">
+              <button
+                type="submit"
+                name="login"
+                className="button -link -primary"
+              >
+                <span>Entrar</span>
+              </button>
             </div>
-          }
-          <div className="form -button m-auto">
-            <button
-              type="submit"
-              name="login"
-              className="button -link -primary"
-            >
-              <span>Entrar</span>
-            </button>
+          </div>
+        </form>
+      </div>
+      { !loginLoading && 
+        <div className="loading -wrap">
+          <div className="loading -anime">
+            <span>Loading...</span>
           </div>
         </div>
-      </form>
-    </div>
+      }
+    </>
   );
 };
 
