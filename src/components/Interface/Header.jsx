0f5f180cc176f23f3df16df2972e5a357d5bd38b
Elton Fagner <eltonfms@gmail.com>
2020-07-08 15:46:09 -0300

    header search component

--- src/components/Interface/Header.jsx
+++ src/components/Interface/Header.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import Loading from '../Interface/Loading';
 import LogoNav from '../../components/Logo/LogoNav';
 
 const Header = () => {
+  const [success, setSuccess] = useState(false);
+  const [loginLoading, setLoginLoading] = useState(false);
+  const { handleSubmit } = useForm();
+
+  const handleSearch = () => {
+    setLoginLoading(true);
+    try {
+      setTimeout(function () {
+        setLoginLoading(false);
+        setSuccess(true);
+      }, 3000);
+    } catch (err) { } finally {  }
+  };
+
+  const onSubmit = () => {
+    handleSearch();
+  };
+
   return (
     <div className="header -wrap">
       <div className="header -logo">
@@ -10,9 +30,21 @@ const Header = () => {
       <div className="header -back">
 
       </div>
-      <div className="header -search">
-
+      <div className="search -wrap">
+        <form onSubmit={handleSubmit(onSubmit)} noValidate>
+          <input
+            type="search"
+            name="search"
+            id="search"
+            placeholder="Pesquisar"
+            autoComplete="off"
+            className="search -input"
+          />
+        </form>
       </div>
+      { loginLoading && 
+        <Loading />
+      }
     </div>
   );
 };
